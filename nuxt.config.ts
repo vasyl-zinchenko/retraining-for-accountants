d398d628e51587d3566048a70ee9a54381143b36
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2023-09-17 01:36:11 +0300

    first commit1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/eslint-module", "nuxt-swiper"],
   eslint: {},
-  generate: {
-    nojekyll: true, //not working on this version
-    fallback: "404.html",
-  },
-  ssr: false, //When false
-  target: "static", // and static, nuxt generates a SPA
 });
